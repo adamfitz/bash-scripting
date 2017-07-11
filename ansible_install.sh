b88912c12a9821c98069bdae3f7a7812f4018297
Adam Fitzgerald <adam@networknoob.net>
2017-07-12 09:50:54 +1000

    simple ansible install script

---
+++ ansible_install.sh
@@ -0,0 +1,9 @@
+printf "Installing Ansible for Debian (run as root user)"
+printf "\n"
+printf "Adding Ubuntu PPA to /etc/apt/sources.list"
+echo deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main >> /etc/apt/sources.list
+printf "Adding keyserver.ubuntu.com"
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C36 -y
+sudo apt-get update
+printf "Installing ansible and prerequisites"
+sudo apt-get install ansible
