c1e1a585d89ad495d5e724f65dcea7c6ce9d7a93
Adam Fitzgerald <adam@networknoob.net>
2017-12-04 22:26:05 +1100

    getting it working...

--- samba_install.sh
+++ samba_install.sh
@@ -9,7 +9,7 @@
 # ======
 # samba_install <samba username> <shared directory>
 #
-# This i
+#
 # Script will check if the given samba username already exists and use this to setup authentication on the given samba share
 # if the given samba username does not exist then the script will create teh user and prompt for a password to set for the user and also set this given password as access to the samba share
 #
@@ -20,7 +20,8 @@ function stop_and_wait()
     }
 
 # install samba
-printf "Installing Samba..."
+printf"\n"
+printf "Installing Samba...\n"
 yum -y install samba samba-client samba-common
 
 # create a user account and set the password
@@ -33,9 +34,14 @@ passwd shareuser
 printf "Creating group datashare...\n"
 groupadd shareuser
 # add the user account to the group share acces group
+printf"\n"
+printf "Adding shareuser account to the datashare group...\n"
 usermod -aG datashare shareuser
+printf"\n"
 
 # setup the samba configuration
+
+# Once this is working, automate writing the smb.conf file based on the input from the user
 
 #[movies]
     #comment = Movies
@@ -47,10 +53,19 @@ usermod -aG datashare shareuser
     #directory mode = 0770
 
 # make the above share directory
+printf"\n"
+printf "Create the directory to share...\n"
 mkdir /mnt/storage/movies
+printf"\n"
 
 #change the owner group of the new directory to the data share group
+printf"\n"
+printf "Changing the owner group of the shared directory...\n"
 chgrp datashare /mnt/storage/movies
+printf"\n"
 
 # set the directory mode on the new directory
+printf"\n"
+printf "Changing share directory permissions to 770...\n"
 chmod 770 /mnt/storage/movies
+printf"\n"
